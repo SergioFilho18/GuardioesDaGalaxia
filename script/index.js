4b77faceea1c0d6cb0f2090889ec8cbac4e06405
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-10-25 10:14:57 -0300

    Added the function about navigation on the pag w the buttons

--- script/index.js
+++ script/index.js
@@ -6,3 +6,37 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0
+const images = document.getElementById('images')
+
+  function changeStatusButtons() {
+
+    let prev = document.getElementById('button_prev')
+
+
+    let next = document.getElementById('button_next')
+
+
+  if(activeMember == 0){
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+  }
+
+  if(activeMember == members.length - 1) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+  }
+  
+}
+
+changeStatusButtons();
+
+function navigationMember(direction) {
+  activeMember = activeMember + direction
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)`;
+  changeStatusButtons();
+}
